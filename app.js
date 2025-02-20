5cd79e4acca8594095a29fe926fbe460983e8c0a
Sean Bryan <seanbryan.noces@gmail.com>
2025-02-20 11:26:51 +0800

    started a new game area and planning to do it. also fixed some minor changes

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ document.getElementById("Submit").onclick = function () {
 
   side1 = parseInt(side1) || 0;
   side2 = parseInt(side2) || 0;
-  let hypotenuse = Math.sqrt(side1 ** 2 + side2 ** 2);
+  let hypotenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
   document.getElementById("output").value = hypotenuse;
 };
