9172d41359c562682f555d67123ab0521cc51f14
Sean Bryan <seanbryan.noces@gmail.com>
2025-02-20 13:19:31 +0800

    created an increment fucntion

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ document.getElementById("Submit").onclick = function () {
   let hypotenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
   document.getElementById("output").value = hypotenuse;
 };
+
+const decrease = document.getElementById("downBtn");
+const increase = document.getElementById("upBtn");
+const reset = document.getElementById("restBtn");
+const output = document.getElementById("outputInc");
+let count = 0;
+
+decrease.onclick = function () {
+  count--;
+  output.textContent = count;
+};
+
+increase.onclick = function () {
+  count++;
+  output.textContent = count;
+};
+reset.onclick = function () {
+  count = 0;
+  output.textContent = count;
+};
